a008f33974a0609118561e9d955bf02211630e6f
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-06-14 20:54:28 -0600

    more wait

--- internal/api/reconcile.go
+++ internal/api/reconcile.go
@@ -116,6 +116,9 @@ func Reconcile(ctx context.Context, c client.Client, scheme *runtime.Scheme, rel
 		if err := util.WaitForCO(ctx, c, logger, "kube-apiserver"); err != nil {
 			return err
 		}
+		if err := util.WaitForCO(ctx, c, logger, "openshift-apiserver"); err != nil {
+			return err
+		}
 		logger.Info("APIServer modified", "OperationResult", op)
 	}
 	return nil
@@ -136,6 +139,9 @@ func Cleanup(ctx context.Context, c client.Client, logger logr.Logger) error {
 		// if we let the finalizer finish before the API server has updated, it will delete a MachineConfig and cause a reboot
 		// if the node reboots before the API server has updated, it can cause the API server to lock up on the next boot
 		if err := util.WaitForCO(ctx, c, logger, "kube-apiserver"); err != nil {
+			return err
+		}
+		if err := util.WaitForCO(ctx, c, logger, "openshift-apiserver"); err != nil {
 			return err
 		}
 		logger.Info("APIServer reverted to original state", "OperationResult", op)
